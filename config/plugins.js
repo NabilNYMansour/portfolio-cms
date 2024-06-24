217ed1254e62c8214bebb32ebc7deefe1cbf68c5
Nabil Mansour <nabilmansour1999@gmail.com>
2024-06-24 00:54:16 -0400

    added sendgrid func

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'noreply@nabilmansour.com',
+        defaultReplyTo: 'replies@nabilmansour.com',
+      },
+    },
+  },
+  // ...
+});
